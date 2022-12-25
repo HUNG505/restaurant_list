22c61187595c8bd68c084c7b3df3628f00564399
HUNG505 <hbkforever@hotmail.com.tw>
2022-12-25 23:05:49 +0800

    feat: add todo seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -9,7 +9,41 @@ const restaurantSchema = new Schema({
     type: String,
     required: true
   },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
   done: {
     type: Boolean
   }
 })
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
