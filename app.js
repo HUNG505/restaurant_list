59dbe6a151c267d64a9280847b8bda593bda7230
HUNG505 <hbkforever@hotmail.com.tw>
2023-01-01 02:12:24 +0800

    add edit function

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get('/restaurants/new', (req, res) => {
 })
 
 // 將新增的資料存資料庫
+// 都是以test輸入也沒問題？
 app.post('/restaurants', (req, res) => {
   const name = req.body.name
   const category = req.body.category
@@ -82,8 +83,6 @@ app.post('/restaurants', (req, res) => {
     .then(res.redirect('/'))
     .catch(error => console.log(error))
 })
-
-
 
 // show頁面路由設定
 app.get('/restaurants/:id', (req, res) => {
@@ -100,6 +99,41 @@ app.get('/restaurants/:id', (req, res) => {
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 
+})
+
+// edit頁面路由設定
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+// 控制edit後的路由
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const category = req.body.category
+  const rating = req.body.rating
+  const location = req.body.location
+  const google_map = req.body.google_map
+  const phone = req.body.phone
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.rating = rating
+      restaurant.location = location
+      restaurant.google_map = google_map
+      restaurant.phone = phone
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
 })
 
 
