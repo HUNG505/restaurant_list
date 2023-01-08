81cd84520950f6453c0b1f0945f002e8091dc65c
HUNG505 <hbkforever@hotmail.com.tw>
2023-01-08 15:41:29 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // 載入express和
 const express = require('express')
-// 載入mongoose
-const mongoose = require('mongoose')
 // 載入handlebars
 const exphbs = require('express-handlebars')
 // 載入Body-parser
 const bodyParser = require('body-parser')
 // 載入method-override
 const methodOverride = require('method-override')
+// 載入mongoose
+require('./config/mongoose')
 
 // 載入總路由器
 const routes = require('./routes/index')
@@ -16,23 +16,10 @@ const app = express()
 // 設定伺服器參數
 const port = 3000
 
-
 // 設定樣版引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // 啟用handlebars這個樣版引擎
 app.set('view engine', 'handlebars')
-
-
-// <---設定資料庫--->
-// 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫的狀態
-const db = mongoose.connection
-
 
 // 設定靜態檔案的位置
 app.use(express.static('public'))
@@ -40,20 +27,9 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 // 使用method-override
 app.use(methodOverride('_method'))
+
 // 將request導入路由器
 app.use(routes)
-
-
-// <---設定路由--->
-// 檢查資料快連線狀態，連線異常
-db.on('error', () => {
-  console.log('mongoDB error!')
-})
-// 檢查資料快連線狀態，連線正常
-db.once('open', () => {
-  console.log('mongoDB connected!')
-})
-
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,32 +1,11 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantSeeders = require('../../restaurant.json').results
+require('../../config/mongoose')
 
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongoDB error!')
-})
 db.once('open', () => {
-  // restaurantSeeders.forEach(restaurant => {
-  //   Restaurant.create({
-  //     name: `${restaurant.name}`,
-  //     name_en: `${restaurant.name_en}`,
-  //     category: `${restaurant.category}`,
-  //     image: `${restaurant.image}`,
-  //     location: `${restaurant.location}`,
-  //     phone: `${restaurant.phone}`,
-  //     google_map: `${restaurant.google_map}`,
-  //     rating: `${restaurant.rating}`,
-  //     description: `${restaurant.description}`
-  //   })
-  // })
+
   Restaurant.create(restaurantSeeders)
     .then(() => {
       console.log('Seed data is done!')
